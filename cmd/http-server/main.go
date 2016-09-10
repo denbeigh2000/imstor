fdb93cc23a3fff07f6a7bddf7ff7d69a456d85be
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2016-09-10 13:15:16 -0700

    Ensured that frontend/API redirection works out-of-the-box

--- cmd/http-server/main.go
+++ cmd/http-server/main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/denbeigh2000/imstor/app"
 	imstorHTTP "github.com/denbeigh2000/imstor/handlers/http"
 	"github.com/denbeigh2000/imstor/stores/disk"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
+const (
+	DefaultAPIPrefix    = "/api"
+	DefaultListenPort   = 8080
+	DefaultFrontendPort = 3000
+)
+
+func frontendURL() *url.URL {
+	port := DefaultFrontendPort
+	uri, err := url.Parse(fmt.Sprintf("http://localhost:%v/", port))
+	if err != nil {
+		panic(err)
+	}
+	return uri
+}
+
+func frontendHandler() http.Handler {
+	uri := frontendURL()
+	return httputil.NewSingleHostReverseProxy(uri)
+}
+
+func apiHandler() http.Handler {
 	//store := memory.NewStore()
 	imageStore := disk.NewStore("./.imstor")
 	thumbStore := disk.NewThumbStore("./.imstor/thumbs")
@@ -20,10 +46,26 @@ func main() {
 
 	httpAPI := imstorHTTP.NewHTTPAPI(imageAPI, thumbAPI, false)
 
+	prefix := DefaultAPIPrefix
+	return http.StripPrefix(prefix, httpAPI)
+}
+
+func main() {
+	fe := frontendHandler()
+	api := apiHandler()
+
+	router := mux.NewRouter()
+	router.PathPrefix("/api/").Handler(api)
+	router.PathPrefix("/").Handler(fe)
+
+	listenURL := fmt.Sprintf(":%v", DefaultListenPort)
+
 	server := http.Server{
-		Addr:    ":8080",
-		Handler: httpAPI,
+		Addr:    listenURL,
+		Handler: router,
 	}
+
+	log.Printf("Listening on %v...\n", listenURL)
 
 	server.ListenAndServe()
 }
